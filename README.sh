f0e2fc17ce324d3e8dd46933f6617958288214ba
Rui Yang <ryang@pivotal.io>
2020-04-03 23:49:36 -0400

    update readme

--- README.sh
+++ README.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # step 1
-git clone https://github.com/vito/dot-nvim.git ~/.config/nvim --recursive
+git clone https://github.com/xtremerui/nvim-config.git ~/.config/nvim --recursive
 
 # there is no step 2
 
